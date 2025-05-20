854982131107bb1cd04d36fbb4e227e85c080d12
RohanKhodade <rohankhodade883@gmail.com>
2025-05-20 22:32:11 +0530

    folder structure and requirements installation

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup,find_packages
+setup(
+    name="src",
+    version="0.0.1",
+    author="Rohan Khodade",
+    author_email="rohankhodade883@gmail.com",
+    packages=find_packages()
+)
